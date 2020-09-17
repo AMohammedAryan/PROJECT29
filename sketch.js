7fb944f20c8a5b8050d5233c246cfcdb1999249f
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-09-17 12:43:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,41 +12,41 @@ var block10, block11, block12, block13, block14, block15, block16, block17, bloc
 var constraint;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1000,400);
 
   engine = Engine.create();
   world = engine.world;
 
-  stand = new Ground(300, 325, 290, 20);
-  stand2 = new Ground(625, 175, 290, 20);
+  stand = new Ground(500, 325, 290, 20);
+  stand2 = new Ground(825, 175, 290, 20);
 
   polygon = new Polygon(50, 100, 50);
 
-  block1 = new Block(300, 295, 50, 50);
-  block2 = new Block(350, 295, 50, 50);
-  block3 = new Block(400, 295, 50, 50);
-  block4 = new Block(250, 295, 50, 50);
-  block5 = new Block(200, 295, 50, 50);
+  block1 = new Block(500, 295, 50, 50);
+  block2 = new Block(550, 295, 50, 50);
+  block3 = new Block(600, 295, 50, 50);
+  block4 = new Block(450, 295, 50, 50);
+  block5 = new Block(400, 295, 50, 50);
 
-  block6 = new Block(300, 245, 50, 50);
-  block7 = new Block(350, 245, 50, 50);
-  block8 = new Block(250, 245, 50, 50);
+  block6 = new Block(500, 245, 50, 50);
+  block7 = new Block(550, 245, 50, 50);
+  block8 = new Block(450, 245, 50, 50);
 
-  block9 = new Block(300, 195, 50, 50);
+  block9 = new Block(500, 195, 50, 50);
 
-  block10 = new Block(625, 145, 50, 50);
-  block11 = new Block(675, 145, 50, 50);
-  block12 = new Block(725, 145, 50, 50);
-  block13 = new Block(575, 145, 50, 50);
-  block14 = new Block(525, 145, 50, 50);
+  block10 = new Block(825, 145, 50, 50);
+  block11 = new Block(875, 145, 50, 50);
+  block12 = new Block(925, 145, 50, 50);
+  block13 = new Block(775, 145, 50, 50);
+  block14 = new Block(725, 145, 50, 50);
 
-  block15 = new Block(625, 95, 50, 50);
-  block16 = new Block(675, 95, 50, 50);
-  block17 = new Block(575, 95, 50, 50);
+  block15 = new Block(825, 95, 50, 50);
+  block16 = new Block(875, 95, 50, 50);
+  block17 = new Block(775, 95, 50, 50);
 
-  block18 = new Block(625, 45, 50, 50);
+  block18 = new Block(825, 45, 50, 50);
 
-  constraint = new SlingShot(polygon.body, {x:50, y:100});
+  constraint = new SlingShot(polygon.body, {x:250, y:100});
 }
 
 function draw() {
